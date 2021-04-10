6afcfff3dba4b778a528faf81e2ac62a13954cde
feng xia <feng_xia41103@hotmail.com>
2021-04-10 08:47:16 -0400

    Add script call to visulization.
    Including the merge frames.

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import os
 from configparser import ConfigParser
 
 import click
-import pandas as pd
 
+from simba.data_plot import data_plot_config
+from simba.gantt import ganntplot_config
+from simba.line_plot import line_plot_config
+from simba.merge_frames_movie import mergeframesPlot
+from simba.path_plot import path_plot_config
+from simba.plot_heatmap import plotHeatMap
+from simba.plot_threshold import plot_threshold
 from simba.process_data_log import analyze_process_data_log
 from simba.process_severity import analyze_process_severity
 from simba.ROI_add_to_features import ROItoFeatures
@@ -13,8 +19,8 @@ from simba.run_RF_model import rfmodel
 from simba.runmodel_1st import validate_model_one_vid_1stStep
 from simba.SimBA import loadprojectini
 from simba.SimBA import outlier_settings
-from simba.SimBA import trainmachinemodel_settings
 from simba.SimBA import video_info_table
+from simba.sklearn_plot_scripts.plot_sklearn_results_2 import plotsklearnresult
 from simba.timeBins_classifiers import time_bins_classifier
 from simba.timeBins_movement import time_bins_movement
 from simba.train_model_2 import trainmodel2
@@ -22,7 +28,6 @@ from simba.validate_model_on_single_video import validate_model_one_vid
 
 
 class MyConfig:
-
     def __init__(self, path_to_ini):
         self.path_to_ini = path_to_ini
         self.config = ConfigParser()
@@ -42,23 +47,18 @@ class MyConfig:
         self.config.set("Frame settings", "mm_per_pixel", str(ppm))
 
     def set_outlier_movement_criterion(self, criterion):
-        self.config.set("Outlier settings",
-                        "movement_criterion", str(criterion))
+        self.config.set("Outlier settings", "movement_criterion", str(criterion))
 
     def set_outlier_location_criterion(self, criterion):
-        self.config.set("Outlier settings",
-                        "location_criterion", str(criterion))
+        self.config.set("Outlier settings", "location_criterion", str(criterion))
 
     def set_outlier_mean_or_median(self, what="mean"):
         self.config.set("Outlier settings", "mean_or_median", str(what))
 
     def set_process_movements(self, body_parts):
         for index, bp in enumerate(body_parts):
-            self.config.set(
-                "process movements", "animal_" + str(index + 1) + "_bp", bp
-            )
-        self.config.set("process movements", "no_of_animals",
-                        str(len(body_parts)))
+            self.config.set("process movements", "animal_" + str(index + 1) + "_bp", bp)
+        self.config.set("process movements", "no_of_animals", str(len(body_parts)))
 
     def set_roi_settings_no_of_animals(self, no):
         self.config.set("ROI settings", "no_of_animals", str(no))
@@ -66,13 +66,8 @@ class MyConfig:
     def set_heat_locations(self, body_part, palette, classifier, bin_size, scale):
         self.config.set("Heatmap location", "body_part", body_part)
         self.config.set("Heatmap location", "Palette", palette)
-        self.config.set(
-            "Heatmap location",
-            "Scale_max_seconds",
-            str(scale))
-        self.config.set(
-            "Heatmap location",
-            "bin_size_pixels", str(bin_size))
+        self.config.set("Heatmap location", "Scale_max_seconds", str(scale))
+        self.config.set("Heatmap location", "bin_size_pixels", str(bin_size))
 
     def get_generated_model_path(self, classifier):
         return self.config.get("SML settings", "model_path_1")
@@ -83,18 +78,18 @@ class MyConfig:
     def get_min_bout_length(self):
         return self.config.get("Minimum_bout_lengths", "min_bout_1")
 
+
 @click.group()
 def cli():
     pass
 
 
 @click.command()
-@click.option("--classifier",
-              required=True,
-              help="Classifier you want to compute w/.")
+@click.option("--classifier", required=True, help="Classifier you want to compute w/.")
 @click.argument("path-to-ini", nargs=1)
 def config(path_to_ini, classifier):
     config = MyConfig(path_to_ini)
+
 
     # set ppm
     config.set_distance_mm(245)
@@ -122,10 +117,10 @@ def config(path_to_ini, classifier):
     l.extractfeatures()
 
     # label behavior
-    # TBD
+    # TBD, manual only so far.
 
     # load metadata
-    t = trainmachinemodel_settings(path_to_ini)
+    #t = trainmachinemodel_settings(path_to_ini)
 
     # TODO: path to meta is hardcoded
     # t.load_choosedata.filePath.set(
@@ -133,21 +128,21 @@ def config(path_to_ini, classifier):
     # t.load_RFvalues()
 
     # train single model
-    # trainmodel2(path_to_ini)
+    trainmodel2(path_to_ini)
 
     # run model
     csv = "/app/output/feng/project_folder/csv/features_extracted/tmp.csv"
     config.read_from_disk()
-    sav = config.get_generated_model_path("mating")
+    sav = config.get_generated_model_path(classifier)
     validate_model_one_vid_1stStep(path_to_ini, csv, sav)
 
     # validate model single vid
     discrimination_threshold = config.get_discrimination_threshold()
     min_bout_length = config.get_min_bout_length()
-    generated_gannt = 0  # 1 to generate
-    # validate_model_one_vid(
-    #    path_to_ini, csv, sav, discrimination_threshold, min_bout_length, 0
-    #)
+    generate_gannt = 0  # 1 to generate
+    validate_model_one_vid(
+        path_to_ini, csv, sav, discrimination_threshold, min_bout_length, generate_gannt
+    )
 
     # run RF model
     rfmodel(path_to_ini)
@@ -171,10 +166,12 @@ def config(path_to_ini, classifier):
     body_part = "Center_1"
     bin_size_pixels = 200
     scale_max_seconds = "auto"
+
+    # values: gnuplot2, plasma, magma, jet, inferno, viridis
     palette = "gnuplot2"
     config.set_heat_locations(
-        body_part, palette, classifier,
-        bin_size_pixels, scale_max_seconds)
+        body_part, palette, classifier, bin_size_pixels, scale_max_seconds
+    )
 
     config.set_roi_settings_no_of_animals(2)
     config.write_to_disk()
@@ -183,19 +180,69 @@ def config(path_to_ini, classifier):
     ROI_process_movement(path_to_ini)
 
     # time bins machine prediction
-    time_bin_size  = 20
+    time_bin_size = 20
     time_bins_classifier(path_to_ini, time_bin_size)
 
     # time bins distance/velocity
     time_bins_movement(path_to_ini, time_bin_size)
 
     # analyze severity
-    severity_scale = 5 # must > 1!
+    severity_scale = 5  # must > 1!
     analyze_process_severity(path_to_ini, severity_scale, classifier)
 
-    #roiAnalysis(path_to_ini, "features_extracted")
+    # plot gannt
+    ganntplot_config(path_to_ini)
 
-    #ROItoFeatures(path_to_ini)
+    # plot sklearn result
+    create_video = 1 # 0 or 1
+    create_frame = 1 # 0 or 1
+    plotsklearnresult(path_to_ini, create_video, create_frame)
+
+    # data plot
+    data_plot_config(path_to_ini, "Centroid")
+
+    # path plot
+    path_plot_config(path_to_ini)
+
+    # distance plot
+    line_plot_config(path_to_ini)
+
+    # heatmap
+    body_part = "Center_1"
+    last_image_only = 0 # 0 or 1
+    plotHeatMap(
+        path_to_ini,
+        body_part,
+        bin_size_pixels,
+        scale_max_seconds,
+        palette,
+        classifier,
+        last_image_only
+    )
+
+    # plot threshold
+    plot_threshold(path_to_ini, classifier)
+
+    # merge frames
+    merging_these = [
+        ("Sklearn",1),
+        ("Gantt",1),
+        ("Path",1),
+        ("'Live' data",1),
+        ("Distance",1),
+        ("Probability",1)
+    ]
+    try:
+        mergeframesPlot(path_to_ini, [x[1] for x in merging_these])
+    except UnboundLocalError:
+        print("Known bug. Ignore.")
+
+    return
+
+
+    # roiAnalysis(path_to_ini, "features_extracted")
+
+    # ROItoFeatures(path_to_ini)
 
 
 cli.add_command(config)
