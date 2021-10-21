9fe2674a0f341b7b1538dde1f52b20dc397a1dbd
feng xia <feng_xia41103@hotmail.com>
2021-10-21 14:24:46 -0400

    refactor: Move video processing func to a class

--- backend/tensorflow/mouse.py
+++ backend/tensorflow/mouse.py
@@ -5,7 +5,8 @@ import os
 import os.path
 import re
 import shutil
-from math import ceil, floor
+from math import ceil
+from math import floor
 from random import randint
 from subprocess import run
 
@@ -22,192 +23,189 @@ CLASS_NAMES = [
 ]
 
 
-def video_to_frame_with_label(output_path, video_file, labels):
-    """Dump video frames to image, then re-org folder based on label.
+class VideoProcessor:
+    def __init__(self, video_file, labels, output_path="."):
+        self.video = video_file
+        self.labels = labels
+        self.output_path = output_path
+        self.all_images = []
 
-    Argument
-    --------
-      video_file: anything ffmpeg can handle.
+        self.image_filename_convention = os.path.join(
+            self.output_path, """%d.png"""
+        )
+        self.image_filename_pat = re.compile(r"\d+(?=.png)")
 
-      labels: list of dict.
+        # probe video
+        probe = ffmpeg.probe(self.video)
+        video_stream = next(
+            (
+                stream
+                for stream in probe["streams"]
+                if stream["codec_type"] == "video"
+            ),
+            None,
+        )
+        self.frame_rate = int(
+            video_stream.get("r_frame_rate", "").split("/")[0]
+        )
+        self.width = int(video_stream["width"])
+        self.height = int(video_stream["height"])
 
-        - name: string, name of the label data
-        - label: CSV, three cols: start time, end time, duration. All
-          in seconds. Frame index will be computed based on the time
-          stamp.
+    def run(self):
+        self.dump_video_to_frame()
+        self._org_images_by_label()
+        self._remove_frame_dumps()
 
-    Return
-    ------
-      path: resulting folder for tf to load
-    """
+    def dump_video_to_frame(self):
+        """Dump video frames to image."""
+        run(["ffmpeg", "-i", self.video, self.image_filename_convention])
 
-    # image filename convention
-    FRAME_IMAGE_FILENAME = os.path.join(output_path, """%d.png""")
-    FILE_PATTERN = re.compile(r"\d+(?=.png)")
-
-    # load video info
-    probe = ffmpeg.probe(video_file)
-    video_stream = next(
-        (
-            stream
-            for stream in probe["streams"]
-            if stream["codec_type"] == "video"
-        ),
-        None,
-    )
-    frame_rate = video_stream.get("r_frame_rate", "").split("/")
-    if len(frame_rate) > 1:
-        frame_rate = int(frame_rate[0])
-    else:
-        frame_rate = None
-        logger.exception("Video has unknown frame rate. Abort.")
-        return
-
-    # extract all frames from video
-    run(["ffmpeg", "-i", video_file, FRAME_IMAGE_FILENAME])
-
-    # get list of all images just extracted
-    all_images = set(
-        [name for name in os.listdir(output_path) if FILE_PATTERN.match(name)]
-    )
-
-    # organize images based on tags
-    for label in labels:
-        # group tagged vs. others
-        tagged_images, untagged_images = categorize_images(
-            all_images, label["name"], label["tags"], frame_rate
+        # get list of all images just extracted
+        self.all_images = set(
+            [
+                name
+                for name in os.listdir(self.output_path)
+                if self.image_filename_pat.match(name)
+            ]
         )
 
-        # move tagged and untagged to its own folders
-        reorg_image_files_on_disk(
-            output_path, label["name"], tagged_images, untagged_images
+    def _categorize_images(self, category_name, tag_timestamps):
+        """Categorize images based on tags.
+
+        Argument
+        --------
+
+          category_name: string, as name
+          tag_timestamps: csv file, has tag info
+
+        Return
+        ------
+
+          tagged_images: set, image file names matching category tag
+          untagged_images: set, non-tagged file names
+
+        """
+
+        # load tag timestamps
+        tagged = []
+        with open(tag_timestamps, newline="") as csvfile:
+            reader = csv.DictReader(
+                csvfile, fieldnames=["start", "end", "duration"]
+            )
+
+            # skip header line
+            next(reader, None)
+
+            # data
+            tagged = [row for row in reader]
+
+        tagged_images = []
+        for t in tagged:
+            start_index = floor(float(t["start"]) * self.frame_rate)
+            end_index = ceil(float(t["end"]) * self.frame_rate)
+            tagged_images += [
+                "{}.png".format(x) for x in range(start_index, end_index + 1)
+            ]
+
+        untagged_images = self.all_images - set(tagged_images)
+        return (set(tagged_images), untagged_images)
+
+    def _reorg_image_files_on_disk(
+        self, tag_name, tagged_images, untagged_images
+    ):
+        """Reorg image files on disk based on tag name.
+
+        For example, if tag name is "grooming", we should expect two
+        folders, `/grooming` and `non-grooming`. Each holds a list of
+        image files according to the tagged_images and untagged_images
+        list.
+
+        Argument
+        --------
+
+          tag_name: as name
+          tagged_images: [string], list of file names. Name has no path.
+          untagged_images: [string], list of file name. Name has no path.
+
+        """
+        # move file to tagged & untagged subfolders
+        non_tagged_name = "non-{}".format(tag_name)
+
+        for p in [tag_name, non_tagged_name]:
+            target = os.path.join(self.output_path, p)
+            if not os.path.exists(target):
+                os.makedirs(target)
+
+        # move files
+        tag_output_path = os.path.join(self.output_path, tag_name)
+        for f in tagged_images:
+            src = os.path.join(self.output_path, f)
+            if os.path.exists(src):
+                shutil.copy(src, tag_output_path)
+
+        untagged_output_path = os.path.join(self.output_path, non_tagged_name)
+        for f in untagged_images:
+            src = os.path.join(self.output_path, f)
+            if os.path.exists(src):
+                shutil.copy(src, untagged_output_path)
+
+    def _org_images_by_label(self):
+        """Organize image based tag/label info.
+
+        Tag file will tell us which image is tagged. The tag will be
+        create as a folder, and its files will be moved into this
+        folder. Once this is constructed, TF can load all data w/ one
+        call.
+
+        Labels can be an array. Each label corresponds to a
+        behavior/tag we want to track. Each label has:
+
+        1. name: the behavior name
+        2. label data file: some sort of data we use to know which
+           image has this behavior, thus we can group the image accordingly.
+        """
+
+        # organize images based on tags
+        for label in self.labels:
+            # group tagged vs. others
+            tagged_images, untagged_images = self._categorize_images(
+                label["name"], label["tags"]
+            )
+
+            # move tagged and untagged to its own folders
+            self._reorg_image_files_on_disk(
+                label["name"], tagged_images, untagged_images
+            )
+
+    def _remove_frame_dumps(self):
+        # delete all frames
+        for i in self.all_images:
+            os.remove(os.path.join(self.output_path, i))
+
+    def video_to_np_array(self):
+        """Load training image & label, and testing image & label.
+
+        Labels are categorization value w/ 1:1 mapping to the image.
+
+        Test data is used to verify the model quality after
+        training. Testing set can have different size from training.
+
+        Return
+        ------
+          np array:
+        """
+
+        # read video
+        out, _ = (
+            ffmpeg.input(self.video)
+            .output("pipe:", format="rawvideo", pix_fmt="rgb24")
+            .run(capture_stdout=True)
         )
 
-    # delete all frames
-    for i in all_images:
-        os.remove(os.path.join(output_path, i))
-
-
-def categorize_images(images, category_name, tag_timestamps, frame_rate):
-    """Categorize images based on tags.
-
-    Argument
-    --------
-
-      images: [string], list of image names
-      category_name: string, as name
-      tag_timestamps: csv file, has tag info
-      frame_rate: int, convert time stamp to frame index
-
-    Return
-    ------
-
-      tagged_images: set, image file names matching category tag
-      untagged_images: set, non-tagged file names
-
-    """
-
-    # load tag timestamps
-    tagged = []
-    with open(tag_timestamps, newline="") as csvfile:
-        reader = csv.DictReader(
-            csvfile, fieldnames=["start", "end", "duration"]
-        )
-
-        # skip header line
-        next(reader, None)
-
-        # data
-        tagged = [row for row in reader]
-
-    tagged_images = []
-    for t in tagged:
-        start_index = floor(float(t["start"]) * frame_rate)
-        end_index = ceil(float(t["end"]) * frame_rate)
-        tagged_images += [
-            "{}.png".format(x) for x in range(start_index, end_index + 1)
-        ]
-
-    untagged_images = images - set(tagged_images)
-    return (set(tagged_images), untagged_images)
-
-
-def reorg_image_files_on_disk(
-    output_path, tag_name, tagged_images, untagged_images
-):
-    """Reorg image files on disk based on tag name.
-
-    For example, if tag name is "grooming", we should expect two
-    folders, `/grooming` and `non-grooming`. Each holds a list of
-    image files according to the tagged_images and untagged_images
-    list.
-
-    Argument
-    --------
-
-      output_path: string, path to images
-      tag_name: as name
-      tagged_images: [string], list of file names. Name has no path.
-      untagged_images: [string], list of file name. Name has no path.
-
-    """
-    # move file to tagged & untagged subfolders
-    non_tagged_name = "non-{}".format(tag_name)
-
-    for p in [tag_name, non_tagged_name]:
-        target = os.path.join(output_path, p)
-        if not os.path.exists(target):
-            os.makedirs(target)
-
-    # copy files
-    tag_output_path = os.path.join(output_path, tag_name)
-    for f in tagged_images:
-        src = os.path.join(output_path, f)
-        if os.path.exists(src):
-            shutil.copy(src, tag_output_path)
-
-    untagged_output_path = os.path.join(output_path, non_tagged_name)
-    for f in untagged_images:
-        src = os.path.join(output_path, f)
-        if os.path.exists(src):
-            shutil.copy(src, untagged_output_path)
-
-
-def video_to_np_array(video_file):
-    """Load training image & label, and testing image & label.
-
-    Labels are categorization value w/ 1:1 mapping to the image.
-
-    Test data is used to verify the model quality after
-    training. Testing set can have different size from training.
-
-    Return
-    ------
-      np array:
-    """
-
-    probe = ffmpeg.probe(video_file)
-    video_stream = next(
-        (
-            stream
-            for stream in probe["streams"]
-            if stream["codec_type"] == "video"
-        ),
-        None,
-    )
-    width = int(video_stream["width"])
-    height = int(video_stream["height"])
-
-    # read video
-    out, _ = (
-        ffmpeg.input(video_file)
-        .output("pipe:", format="rawvideo", pix_fmt="rgb24")
-        .run(capture_stdout=True)
-    )
-
-    # convert to np array
-    video = np.frombuffer(out, np.uint8).reshape([-1, height, width, 3])
-    return video
+        # convert to np array
+        video = np.frombuffer(out, np.uint8).reshape(
+            [-1, self.height, self.width, 3])
+        return video
 
 
 def load_data(path, subset, validation_split, batch_size, height, width):
@@ -363,7 +361,9 @@ def get_dataset(video_file, labels):
     output_path = os.path.split(video_file)[0]
 
     # video frame images
-    video_to_frame_with_label(output_path, video_file, labels)
+    video_processor = VideoProcessor(video_file, labels, output_path)
+    video_processor.run()
+    1/0
 
     # load images as dataset
     train_ds = load_data(
